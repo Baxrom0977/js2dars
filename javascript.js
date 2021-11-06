d9ca9c80472d774dce408a606c71e0f99ad1e22f
java <java>
2021-11-06 22:07:33 +0500

    newjs

--- javascript.js
+++ javascript.js
@@ -1,44 +1,69 @@
 var ism = prompt('Ismingizni kiriting');
 console.log( ' Mening ismim ' +ism);
-var age = prompt (' Yoshingizni kiriting ');
+var age = +prompt (' Yoshingizni kiriting ');
 console.log( ' Mening yoshim '+age+" da" );
 
 var misol1 = +prompt( " 15 + 31 =  ");
-console.log( ' 1-misol: '+" tog'ri javob; 15 + 31 = " + (15 + 31) + ' sizning javob: ' + misol1   ) ;
+var misol1_a = 15;
+var misol1_b = 31;
+console.log( ' 1-misol: '+" tog'ri javob; 15 + 31 = " + (misol1_a + misol1_b) + ' sizning javob: ' + misol1   ) ;
 
 var misol2 = +prompt( " 48 - 22 =  ");
-console.log( ' 2-misol: '+" tog'ri javob; 48 - 22 = " + (48 - 22) + ' sizning javob: ' + misol2   ) ;
+var misol2_a = 48;
+var misol2_b = 22;
+console.log( ' 2-misol: '+" tog'ri javob; 48 - 22 = " + (misol2_a - misol2_b) + ' sizning javob: ' + misol2   ) ;
 
 var misol3 = +prompt( " 8 * 15 =  ");
-console.log( ' 3-misol: '+" tog'ri javob; 8 * 15 = " + (8 * 15) + ' sizning javob: ' + misol3   ) ;
+var misol3_a = 8;
+var misol3_b = 15;
+console.log( ' 3-misol: '+" tog'ri javob; 8 * 15 = " + (misol3_a * misol3_b) + ' sizning javob: ' + misol3   ) ;
 
 var misol4 = +prompt( " 100 / 25 =  ");
-console.log( ' 4-misol: '+" tog'ri javob; 100 / 25 = " + (100 / 25) + ' sizning javob: ' + misol4   ) ;
+var misol4_a = 100;
+var misol4_b = 25;
+console.log( ' 4-misol: '+" tog'ri javob; 100 / 25 = " + (misol4_a / misol4_b) + ' sizning javob: ' + misol4   ) ;
 
 var misol5 = +prompt( " 200 - 15 =  ");
-console.log( ' 5-misol: '+" tog'ri javob; 200 - 15 = " + (200 - 15) + ' sizning javob: ' + misol5   ) ;
+var misol5_a = 200;
+var misol5_b = 15;
+console.log( ' 5-misol: '+" tog'ri javob; 200 - 15 = " + (misol5_a - misol5_b) + ' sizning javob: ' + misol5   ) ;
 
 var misol6 = +prompt( " 189 + 75 =  ");
-console.log( ' 6-misol: '+" tog'ri javob; 189 + 75 = " + (189 + 75) + ' sizning javob: ' + misol6   ) ;
+var misol6_a = 189;
+var misol6_b = 75;
+console.log( ' 6-misol: '+" tog'ri javob; 189 + 75 = " + (misol6_a+misol6_b) + ' sizning javob: ' + misol6   ) ;
 
 var misol7 = +prompt( " 15 * 40 =  ");
-console.log( ' 7-misol: '+" tog'ri javob; 15 * 40 = " + (15 * 40) + ' sizning javob: ' + misol7   ) ;
+var misol7_a = 15;
+var misol7_b = 40;
+console.log( ' 7-misol: '+" tog'ri javob; 15 * 40 = " + (misol7_a * misol7_b) + ' sizning javob: ' + misol7   ) ;
 
 var misol8 = +prompt( " 72 / 8 =  ");
+var misol8_a = 72;
+var misol8_b = 8;
 console.log( ' 8-misol: '+" tog'ri javob; 72 / 8 = " + (72 / 8) + ' sizning javob: ' + misol8   ) ;
 
 var misol9 = +prompt( " 1985 - 632 =  ");
+var misol9_a = 1985;
+var misol9_b = 632;
 console.log( ' 8-misol: '+" tog'ri javob; 1985 - 632 = " + (1985 - 632) + ' sizning javob: ' + misol9   ) ;
 
 var misol10 = +prompt( " 156998 + 515451 =  ");
+var misol10_a = 156998;
+var misol10_b = 515451;
 console.log( ' 10-misol: '+" tog'ri javob; 156998 + 515451 = " + (156998 + 515451) + ' sizning javob: ' + misol10   ) ;
 
 var misol11 = +prompt( " 64 * 23 =  ");
+var misol11_a = 64;
+var misol11_b = 23;
 console.log( ' 11-misol: '+" tog'ri javob; 64 * 23 = " + (64 * 23) + ' sizning javob: ' + misol11   ) ;
 
 var misol12 = +prompt( " 100 % 30 =  ");
-console.log( ' 12x-misol: '+" tog'ri javob; 100 % 30 = " + (100 % 30) + ' sizning javob: ' + misol12   ) ;
+var misol12_a = 100;
+var misol12_b = 30;
+console.log( ' 12-misol: '+" tog'ri javob; 100 % 30 = " + (misol12_a % misol12_b) + ' sizning javob: ' + misol12   ) ;
 
+alert("siz matematika boyicha 1 o'rinni oldingiz shuning uchun sizga maqtov yorlig'i ")
 
 
 
