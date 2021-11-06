662921178fc797aff0bac9aee9fd9b4b279ca53e
baxrom <baxrom>
2021-11-06 16:00:01 +0500

    script git commit -m script

---
+++ javascript.js
@@ -0,0 +1,47 @@
+var ism = prompt('Ismingizni kiriting');
+console.log( ' Mening ismim ' +ism);
+var age = prompt (' Yoshingizni kiriting ');
+console.log( ' Mening yoshim '+age+" da" );
+
+var misol1 = +prompt( " 15 + 31 =  ");
+console.log( ' 1-misol: '+" tog'ri javob; 15 + 31 = " + (15 + 31) + ' sizning javob: ' + misol1   ) ;
+
+var misol2 = +prompt( " 48 - 22 =  ");
+console.log( ' 2-misol: '+" tog'ri javob; 48 - 22 = " + (48 - 22) + ' sizning javob: ' + misol2   ) ;
+
+var misol3 = +prompt( " 8 * 15 =  ");
+console.log( ' 3-misol: '+" tog'ri javob; 8 * 15 = " + (8 * 15) + ' sizning javob: ' + misol3   ) ;
+
+var misol4 = +prompt( " 100 / 25 =  ");
+console.log( ' 4-misol: '+" tog'ri javob; 100 / 25 = " + (100 / 25) + ' sizning javob: ' + misol4   ) ;
+
+var misol5 = +prompt( " 200 - 15 =  ");
+console.log( ' 5-misol: '+" tog'ri javob; 200 - 15 = " + (200 - 15) + ' sizning javob: ' + misol5   ) ;
+
+var misol6 = +prompt( " 189 + 75 =  ");
+console.log( ' 6-misol: '+" tog'ri javob; 189 + 75 = " + (189 + 75) + ' sizning javob: ' + misol6   ) ;
+
+var misol7 = +prompt( " 15 * 40 =  ");
+console.log( ' 7-misol: '+" tog'ri javob; 15 * 40 = " + (15 * 40) + ' sizning javob: ' + misol7   ) ;
+
+var misol8 = +prompt( " 72 / 8 =  ");
+console.log( ' 8-misol: '+" tog'ri javob; 72 / 8 = " + (72 / 8) + ' sizning javob: ' + misol8   ) ;
+
+var misol9 = +prompt( " 1985 - 632 =  ");
+console.log( ' 8-misol: '+" tog'ri javob; 1985 - 632 = " + (1985 - 632) + ' sizning javob: ' + misol9   ) ;
+
+var misol10 = +prompt( " 156998 + 515451 =  ");
+console.log( ' 10-misol: '+" tog'ri javob; 156998 + 515451 = " + (156998 + 515451) + ' sizning javob: ' + misol10   ) ;
+
+var misol11 = +prompt( " 64 * 23 =  ");
+console.log( ' 11-misol: '+" tog'ri javob; 64 * 23 = " + (64 * 23) + ' sizning javob: ' + misol11   ) ;
+
+var misol12 = +prompt( " 100 % 30 =  ");
+console.log( ' 12x-misol: '+" tog'ri javob; 100 % 30 = " + (100 % 30) + ' sizning javob: ' + misol12   ) ;
+
+
+
+
+
+
+
