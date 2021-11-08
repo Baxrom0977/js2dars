17788018c41605ddaa3bfa92716bf4d996f2b63c
baxrom <baxrom>
2021-11-08 22:59:11 +0500

    js2dars

--- javascript.js
+++ javascript.js
@@ -1,72 +1,53 @@
 var ism = prompt('Ismingizni kiriting');
-console.log( ' Mening ismim ' +ism);
-var age = +prompt (' Yoshingizni kiriting ');
-console.log( ' Mening yoshim '+age+" da" );
+console.log(' Mening ismim ' + ism);
+var age = +prompt(' Yoshingizni kiriting ');
+console.log(' Mening yoshim ' + age + " da");
 
-var misol1 = +prompt( " 15 + 31 =  ");
-var misol1_a = 15;
-var misol1_b = 31;
-console.log( ' 1-misol: '+" tog'ri javob; 15 + 31 = " + (misol1_a + misol1_b) + ' sizning javob: ' + misol1   ) ;
+var a = 8
+var b = 5
+var c = 4
+var d = 3
+var e = 6
+var f = 10
 
-var misol2 = +prompt( " 48 - 22 =  ");
-var misol2_a = 48;
-var misol2_b = 22;
-console.log( ' 2-misol: '+" tog'ri javob; 48 - 22 = " + (misol2_a - misol2_b) + ' sizning javob: ' + misol2   ) ;
+var misol1 = +prompt(" 8 + 5 =  ");
+javob1 = a + b
+if (misol1 == javob1) {
+    console.log(" Siz to'g'ri javob berdingiz " + javob1);
+} else {
+    console.log(" Sizning javob noto'g'ri " + misol1 + " , To'g'ri javob " + javob1);
+}
 
-var misol3 = +prompt( " 8 * 15 =  ");
-var misol3_a = 8;
-var misol3_b = 15;
-console.log( ' 3-misol: '+" tog'ri javob; 8 * 15 = " + (misol3_a * misol3_b) + ' sizning javob: ' + misol3   ) ;
+var misol2 = +prompt(" 8 - 5 =  ");
+javob2 = a - b
+if (misol2 == javob2) {
+    console.log(" Siz to'g'ri javob berdingiz " + javob2);
+} else {
+    console.log(" Sizning javob noto'g'ri " + misol2 + " , To'g'ri javob " + javob2);
+}
 
-var misol4 = +prompt( " 100 / 25 =  ");
-var misol4_a = 100;
-var misol4_b = 25;
-console.log( ' 4-misol: '+" tog'ri javob; 100 / 25 = " + (misol4_a / misol4_b) + ' sizning javob: ' + misol4   ) ;
+var misol3 = +prompt(" 10 * 4 =  ");
+javob3 = f * c
+if (misol3 == javob3) {
+    console.log(" Siz to'g'ri javob berdingiz " + javob3);
+} else {
+    console.log(" Sizning javob noto'g'ri " + misol3 + " , To'g'ri javob " + javob3);
+}
 
-var misol5 = +prompt( " 200 - 15 =  ");
-var misol5_a = 200;
-var misol5_b = 15;
-console.log( ' 5-misol: '+" tog'ri javob; 200 - 15 = " + (misol5_a - misol5_b) + ' sizning javob: ' + misol5   ) ;
+var misol4 = +prompt(" 6 / 3 =  ");
+javob4 = e / d
+if (misol4 == javob4) {
+    console.log(" Siz to'g'ri javob berdingiz " + javob4);
+} else {
+    console.log(" Sizning javob noto'g'ri " + misol4 + " , To'g'ri javob " + javob4);
+}
 
-var misol6 = +prompt( " 189 + 75 =  ");
-var misol6_a = 189;
-var misol6_b = 75;
-console.log( ' 6-misol: '+" tog'ri javob; 189 + 75 = " + (misol6_a+misol6_b) + ' sizning javob: ' + misol6   ) ;
-
-var misol7 = +prompt( " 15 * 40 =  ");
-var misol7_a = 15;
-var misol7_b = 40;
-console.log( ' 7-misol: '+" tog'ri javob; 15 * 40 = " + (misol7_a * misol7_b) + ' sizning javob: ' + misol7   ) ;
-
-var misol8 = +prompt( " 72 / 8 =  ");
-var misol8_a = 72;
-var misol8_b = 8;
-console.log( ' 8-misol: '+" tog'ri javob; 72 / 8 = " + (72 / 8) + ' sizning javob: ' + misol8   ) ;
-
-var misol9 = +prompt( " 1985 - 632 =  ");
-var misol9_a = 1985;
-var misol9_b = 632;
-console.log( ' 8-misol: '+" tog'ri javob; 1985 - 632 = " + (1985 - 632) + ' sizning javob: ' + misol9   ) ;
-
-var misol10 = +prompt( " 156998 + 515451 =  ");
-var misol10_a = 156998;
-var misol10_b = 515451;
-console.log( ' 10-misol: '+" tog'ri javob; 156998 + 515451 = " + (156998 + 515451) + ' sizning javob: ' + misol10   ) ;
-
-var misol11 = +prompt( " 64 * 23 =  ");
-var misol11_a = 64;
-var misol11_b = 23;
-console.log( ' 11-misol: '+" tog'ri javob; 64 * 23 = " + (64 * 23) + ' sizning javob: ' + misol11   ) ;
-
-var misol12 = +prompt( " 100 % 30 =  ");
-var misol12_a = 100;
-var misol12_b = 30;
-console.log( ' 12-misol: '+" tog'ri javob; 100 % 30 = " + (misol12_a % misol12_b) + ' sizning javob: ' + misol12   ) ;
+var misol5 = +prompt(" 10 % 3 =  ");
+javob5 = f % d
+if (misol5 == javob5) {
+    console.log(" Siz to'g'ri javob berdingiz " + javob5);
+} else {
+    console.log(" Sizning javob noto'g'ri " + misol5 + " , To'g'ri javob " + javob5);
+}
 
 alert("siz matematika boyicha 1 o'rinni oldingiz shuning uchun sizga maqtov yorlig'i ")
-
-
-
-
-
-
